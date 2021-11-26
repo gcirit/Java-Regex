b0d25312fda264518a2c31c0f633c7c841b033c8
Göktürk Cirit <51999685+gcirit@users.noreply.github.com>
2021-11-26 23:29:07 +0300

    Add files via upload

---
+++ src/regexPatterns/RegexExample3.java
@@ -0,0 +1,11 @@
+package regexPatterns;
+
+import java.util.regex.Pattern;
+
+class RegexExample3 {
+	public static void main(String args[]) {
+		System.out.println(Pattern.matches("[amn]", "abcd"));// false (not a or m or n)
+		System.out.println(Pattern.matches("[amn]", "a"));// true (among a or m or n)
+		System.out.println(Pattern.matches("[amn]", "ammmna"));// false (m and a comes more than once)
+	}
+}
